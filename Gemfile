77314670911770bf25ce89c4ff6d62c29427eb60
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2021-07-24 14:32:31 -0300

    Aplly tests

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem "rspec"
   gem 'rspec-rails', ">= 3.9.0"
+  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 end
 
 group :test do
---
+++ spec/requests/videos_spec.rb
@@ -0,0 +1,123 @@
+require 'rails_helper'
+
+RSpec.describe "/videos", type: :request do
+  let(:valid_attributes) {
+    {
+      title: Faker::Superhero.descriptor,
+      description: Faker::Lorem.sentence(word_count: 9),
+      url: Faker::Internet.url
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      title: '',
+      description: '',
+      url: '',
+    }
+  }
+
+  let(:valid_headers) {
+    {}
+  }
+
+  describe "GET /index" do
+    it "renders a successful response" do
+      Video.create! valid_attributes
+      get videos_url, headers: valid_headers, as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      video = Video.create! valid_attributes
+      get video_url(video), as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Video" do
+        expect {
+          post videos_url,
+               params: { video: valid_attributes }, headers: valid_headers, as: :json
+        }.to change(Video, :count).by(1)
+      end
+
+      it "renders a JSON response with the new video" do
+        post videos_url,
+             params: { video: valid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including("application/json; charset=utf-8"))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Video" do
+        expect {
+          post videos_url,
+               params: { video: invalid_attributes }, as: :json
+        }.to change(Video, :count).by(0)
+      end
+
+      it "renders a JSON response with errors for the new video" do
+        post videos_url,
+             params: { video: invalid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        {
+          title: Faker::Superhero.descriptor,
+          description: Faker::Lorem.sentence(word_count: 9),
+          url: Faker::Internet.url
+        }
+      }
+
+      it "updates the requested video" do
+        video = Video.create! valid_attributes
+        patch video_url(video),
+              params: { video: new_attributes }, headers: valid_headers, as: :json
+        video.reload
+
+        expect(video.title).to eq(new_attributes[:title])
+        expect(video.description).to eq(new_attributes[:description])
+        expect(video.url).to eq(new_attributes[:url])
+      end
+
+      it "renders a JSON response with the video" do
+        video = Video.create! valid_attributes
+        patch video_url(video),
+              params: { video: new_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to match(a_string_including("application/json; charset=utf-8"))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders a JSON response with errors for the video" do
+        video = Video.create! valid_attributes
+        patch video_url(video),
+              params: { video: invalid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested video" do
+      video = Video.create! valid_attributes
+      expect {
+        delete video_url(video), headers: valid_headers, as: :json
+      }.to change(Video, :count).by(-1)
+    end
+  end
+end
